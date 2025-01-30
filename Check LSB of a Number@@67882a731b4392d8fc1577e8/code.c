b2ce1f0c8a1f06e013f70ce5694b279b86554398
samar <167969208+Samar0607@users.noreply.github.com>
2025-01-30 11:36:43 +0530

    Solution to Check LSB of a Number

---
+++ Check LSB of a Number@@67882a731b4392d8fc1577e8/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int num1;
+    scanf("%d",&num1);
+    printf("%d",num1 & 1);
+    return 0;
+}
